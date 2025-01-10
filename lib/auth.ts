508e602286053337426a725cde6e33cb4613ba45
ChandraShekhar <csyadav0513@gmail.com>
2025-01-10 12:23:07 +0530

    Add authentication options using next-auth with credentials provider

---
+++ lib/auth.ts
@@ -0,0 +1,55 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User.model";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        try {
+          await connectToDatabase();
+          // 1) Find user by email
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          // 2) Check password
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValid) {
+            throw new Error("Invalid password");
+          }
+
+          // 3) Return user
+          return {
+            id: user.id,
+            email: user.email,
+            role: user.role,
+          };
+        } catch (error) {
+          console.error("Auth Error", error);
+          throw error;
+        }
+      },
+    }),
+  ],
+};
